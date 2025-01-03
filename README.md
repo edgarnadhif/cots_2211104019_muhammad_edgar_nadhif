574d02f7abb0e6a278a77bed7d591a3f708c3368
edgarnadhif <edgarndf345@gmail.com>
2025-01-03 14:44:10 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# cots_2211104019_muhammad_edgar_nadhif
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/cots_2211104019_muhammad_edgar_nadhif/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.cots_2211104019_muhammad_edgar_nadhif
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
